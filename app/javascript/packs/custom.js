685e9cc82a32e6130ac7d6db5ef80e47247e40c4
Toby Varland <toby.varland@varland.com>
2020-08-12 18:46:25 -0400

    Scroll to filter panel when toggling and remove bad shop order PDF links

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,2 +1,28 @@
 $(function() { $('[data-toggle="tooltip"]').tooltip(); });
 $(function() { $('[data-toggle="popover"]').popover(); });
+
+var LinkValidator = {
+  selector: '.shop-order-pdf',
+  validateLinks: function() {
+    $(LinkValidator.selector).each(function() {
+      var link = $(this);
+      $.ajax({
+        type: 'get',
+        url: link.attr("href"),
+        error: function() {
+          link.remove();
+        },
+      });
+    })
+  }
+};
+
+$(function() {
+  LinkValidator.validateLinks();
+  $('.filters-button').on('click', function() {
+    var target = $(this).data('target');
+    $('html,body').animate({
+      scrollTop: $(target).offset().top
+    }, 'fast');
+  });
+});
